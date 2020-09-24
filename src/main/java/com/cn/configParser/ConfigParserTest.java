f5aeda29e247b3be491b83c7d3262f144cc13001
hels <1597445096@qq.com>
2020-09-24 18:32:47 +0800

    propertySource解析测试

--- src/main/java/com/cn/configParser/ConfigParserTest.java
+++ src/main/java/com/cn/configParser/ConfigParserTest.java
@@ -5,6 +5,9 @@ import com.cn.configParser.componentScanTest.Student;
 import com.cn.configParser.importAndBeanTest.Apple;
 import com.cn.configParser.importAndBeanTest.ImportConfig;
 import com.cn.configParser.importAndBeanTest.location.Banana;
+import com.cn.configParser.propertySourceConfig.scanPackage.DBConnection;
+import com.cn.configParser.propertySourceConfig.EnvironmentPropertySourceConfig;
+import com.cn.configParser.propertySourceConfig.TestBean;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -34,5 +37,25 @@ public class ConfigParserTest {
         System.out.println(apple.getName() + "; " + apple.getColor() + "; " + apple.getTaste());
         Banana banana = (Banana) context.getBean("banana");
         System.out.println(banana.getName() + "; " + banana.getProducingArea());
+    }
+
+    /**
+     * 测试注解@PropertySource与Environment一起用
+     */
+    @Test
+    public void testPropertySourceAndEnvironment() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EnvironmentPropertySourceConfig.class);
+        TestBean tb = (TestBean) context.getBean("testBean");
+        System.out.println(tb.toString());
+    }
+
+    /**
+     * 测试注解@PropertySource与@Value一起用
+     */
+    @Test
+    public void testPropertySourceAndValue() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.cn.configParser.propertySourceConfig.scanPackage");
+        DBConnection dbConnection =  context.getBean(DBConnection.class);
+        dbConnection.printDBConfigs();
     }
 }
