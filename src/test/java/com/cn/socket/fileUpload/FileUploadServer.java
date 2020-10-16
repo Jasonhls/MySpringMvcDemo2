65f4227818579724d1f18446b1e6efedd7d30411
hels <1597445096@qq.com>
2020-10-16 14:15:14 +0800

    利用TCP通信简单实现文件的上传下载功能

--- src/test/java/com/cn/socket/fileUpload/FileUploadServer.java
+++ src/test/java/com/cn/socket/fileUpload/FileUploadServer.java
@@ -13,7 +13,7 @@ public class FileUploadServer {
 
         ServerSocket serverSocket = new ServerSocket(8888);
         while(true) {
-            Socket socket = serverSocket.accept();
+            Socket socket = serverSocket.accept();//main启动后，程序会阻塞在这里，知道有客户端发来请求的数据，才会往下继续执行
             new Thread(new Uploader(socket)).start();
         }
     }
