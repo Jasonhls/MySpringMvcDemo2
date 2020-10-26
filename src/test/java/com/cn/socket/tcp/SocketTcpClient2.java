6f71158f7e8f55919ecc83ceabf53d1a7e94fe10
hels <1597445096@qq.com>
2020-10-26 16:12:41 +0800

    使用字符输入输出流测试socket通信

--- src/test/java/com/cn/socket/tcp/SocketTcpClient2.java
+++ src/test/java/com/cn/socket/tcp/SocketTcpClient2.java
@@ -1,8 +1,9 @@
 package com.cn.socket.tcp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -13,7 +14,7 @@ import java.net.SocketAddress;
  * @author: helisen
  * @create: 2020-07-25 10:26
  **/
-public class SocketTcpClient {
+public class SocketTcpClient2 {
 	public static void main(String[] args) throws IOException {
 		//创建socket
 		final Socket socket = new Socket();
@@ -21,22 +22,18 @@ public class SocketTcpClient {
 		SocketAddress address = new InetSocketAddress(InetAddress.getLocalHost(), 8080);
 		socket.connect(address);
 		//向服务器发送的内容
-		OutputStream os = socket.getOutputStream();
+		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 		String sendStr = "客户端问服务器端：你们在吗";
-		os.write(sendStr.getBytes());
-		os.flush();
+		out.write(sendStr);
+		out.flush();
 		//等待服务器端响应
-		InputStream is = socket.getInputStream();
-		byte[] bytes = new byte[1024];
-		int len;
-		//这里必须用if，如果用while就会阻塞在这里，一直等待
-		if((len = is.read(bytes)) != -1) {
-			System.out.println("服务器端回复：" + new String(bytes, 0, len));
-		}
-
+		System.out.println("客户端已经发送了消息");
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String s = in.readLine();
+		System.out.println("服务器端回复：" + s);
 		//关闭连接
-		is.close();
-		os.close();
+		in.close();
+		out.close();
 		socket.close();
 	}
 }
