d3e61bac69e6464951615c28209370c2c2e1a105
hels <1597445096@qq.com>
2021-01-28 15:14:04 +0800

    测试添加自定义的HttpMessageConverter

--- src/main/java/com/cn/configurer/MyWebAppConfigurer.java
+++ src/main/java/com/cn/configurer/MyWebAppConfigurer.java
@@ -1,10 +1,19 @@
 package com.cn.configurer;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.cn.handler.MyHandlerOne;
 import com.cn.handler.MyHandlerTwo;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @description:
@@ -17,5 +26,25 @@ public class MyWebAppConfigurer extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyHandlerOne()).addPathPatterns("/abc");
         registry.addInterceptor(new MyHandlerTwo()).addPathPatterns("def");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        List<MediaType> supportMediaTypeList = new ArrayList<>();
+        supportMediaTypeList.add(MediaType.APPLICATION_JSON_UTF8);
+
+        FastJsonConfig config = new FastJsonConfig();
+        config.setSerializerFeatures(
+                SerializerFeature.WriteMapNullValue, //保留空的字段
+                SerializerFeature.WriteNullStringAsEmpty, // String null -> ""
+                SerializerFeature.WriteNullNumberAsZero, // Number null -> 0
+                SerializerFeature.WriteNullListAsEmpty, // List null -> []
+                SerializerFeature.WriteNullBooleanAsFalse // Boolean null -> false
+        );
+        converter.setFastJsonConfig(config);
+        converter.setSupportedMediaTypes(supportMediaTypeList);
+        converter.setCharset(Charset.forName("UTF-8"));
+        converters.add(converter);
     }
 }
