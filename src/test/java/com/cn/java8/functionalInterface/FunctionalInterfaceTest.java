b2c5f3fd868c6228bf5d795112adc2bfbaf679b4
hels <1597445096@qq.com>
2021-03-24 18:58:19 +0800

    学习FunctionalInterface

---
+++ src/test/java/com/cn/java8/functionalInterface/FunctionalInterfaceTest.java
@@ -0,0 +1,15 @@
+package com.cn.java8.functionalInterface;
+
+import org.junit.Test;
+
+/**
+ * @description:
+ * @author: helisen
+ * @create: 2021-03-24 18:07
+ **/
+public class FunctionalInterfaceTest {
+    @Test
+    public void test() {
+
+    }
+}
---
+++ src/test/java/com/cn/java8/functionalInterface/GreetingService.java
@@ -0,0 +1,11 @@
+package com.cn.java8.functionalInterface;
+
+/**
+ * @description:
+ * @author: helisen
+ * @create: 2021-03-24 18:05
+ **/
+@FunctionalInterface
+public interface GreetingService {
+    void sayMessage(String message);
+}
